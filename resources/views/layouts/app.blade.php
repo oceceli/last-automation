eea7e43d637eacdeaa7247b24bbebf80e8cbefd0
Ömer C <oceceli@hotmail.com>
2020-09-22 18:15:57 +0300

    güncel

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,21 +20,18 @@
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
-            @livewire('navigation-dropdown')
 
             <!-- Page Heading -->
-            <header class="bg-white shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header }}
-                    app blade içi
-                    <livewire:counter>
-                </div>
-            </header>
+            @livewire('navigation-dropdown')
+
 
             <!-- Page Content -->
             <main>
                 {{ $slot }}
             </main>
+
+            {{ $test }}
+
         </div>
 
         @stack('modals')
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/deneme', function () {
+    return view('deneme');
+}); 
+
 
 
 Route::middleware('auth')->group(function () {
