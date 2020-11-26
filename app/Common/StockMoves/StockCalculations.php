e28a85bb37b5a84c43c970751f9f09f809665bfe
Ömer C <oceceli@hotmail.com>
2020-11-26 16:46:52 +0300

    stockcalculations class

--- app/Common/StockMoves/StockCalculations.php
+++ app/Common/StockMoves/StockCalculations.php
@@ -9,23 +9,64 @@ class StockCalculations
 {
     public function calculateTotal()
     {
-        $productsHasMoves = Product::wherehas('stockmoves')->get();
-        // foreach ($productsHasMoves as $product) {
-        //     $a = StockMove::where()
-        // }
-        $a = StockMove::where('product_id', 2)
-                    ->where('direction', true)
-                    ->sum('base_amount');
-        dd($a);
+        foreach (Product::all() as $product) {
+            $a[] = [
+                'product' => $product,
+                'total' => StockMove::where(['product_id' => $product->id])->sum('base_amount'),
+            ];
+        }
+        dd(($a));
     }
 
-    public function totalProductionGross($productId)
+    private function upDirectionAmount($productId)
     {
-        
+        return StockMove::where([
+            'product_id' => $productId,
+            'direction' => true,
+        ]);
     }
 
-    public function totalProductionWaste($productId)
-    {
 
+
+    
+    public function calculateTotallllll()
+    {
+        $products = Product::wherehas('stockmoves')->where('producible', 1)->get();
+        foreach ($products as $product) {
+            $a[] = [
+                'product' => $product,
+                'total' => $this->totalProductionGross($product->id) - $this->totalProductionWaste($product->id),
+                // 'waste' => $this->totalProductionWaste($product->id),
+                'lots' => $this->lotNumbers($product->id),
+            ];
+        }
+        dd(($a));
+    }
+
+    private function totalProductionGross($productId)
+    {
+        return StockMove::where([
+                'product_id' => $productId, 
+                'direction' => true,
+                'type' => 'production_gross',
+            ]) ->sum('base_amount');
+    }
+
+    private function totalProductionWaste($productId)
+    {
+        return StockMove::where([
+                'product_id' => $productId, 
+                'direction' => false,
+                'type' => 'production_waste',
+            ])->sum('base_amount');
+    }
+
+    private function lotNumbers($productId)
+    {
+        StockMove::where([
+            'product_id' => $productId,
+            'direction' => true,
+            'lot_number' => 'production_gross'
+        ])->sum('base_amount');
     }
 }
