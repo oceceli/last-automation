e00081a9f582897aecd03290c07c788b4ec23827
Ömer C <oceceli@hotmail.com>
2020-12-16 13:11:56 +0300

    lang for paginator

--- resources/lang/tr/datatable.php
+++ resources/lang/tr/datatable.php
@@ -3,4 +3,5 @@
 return [
     'perpage' => 'Syf.',
     'perpage_explain' => 'Sayfa başına gösterilecek veri adedini ifade eder.',
+    'showing_firstItem_to_lastItem_of_total_results' => ':total sonuçtan :firstItem ile :lastItem arası gösteriliyor',
 ];
