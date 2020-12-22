d49eebe690559728e8dd6458b7b19751f707697a
Ömer C <oceceli@hotmail.com>
2020-12-22 13:41:19 +0300

    tooltip centered

--- resources/views/components/table-toolbar.blade.php
+++ resources/views/components/table-toolbar.blade.php
@@ -1,7 +1,7 @@
 
 
 <div class="bg-white border-t border-r border-l rounded-t-md p-4 flex justify-between items-center">
-    <div class="ui tiny icon input w-28 border-green-500" data-tooltip="{{ __('datatable.perpage_explain') }}" data-position="top left" data-variation="tiny wide fixed">
+    <div class="ui tiny icon input w-28 border-green-500" data-tooltip="{{ __('datatable.perpage_explain') }}" data-position="right center" data-variation="tiny wide fixed">
         <i class="stream icon"></i>
         <input wire:model.debounce.300ms="perPage" type="number" value="{{ $perPage }}" placeholder="{{ __('datatable.perpage') }}">
     </div>
