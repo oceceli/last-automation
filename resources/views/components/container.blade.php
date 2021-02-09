7d693f5a53743a9a15d02b2ad16ead2ea8a28601
Ömer C <oceceli@hotmail.com>
2021-02-09 23:45:15 +0300

    container has no effect before md screens

--- resources/views/components/container.blade.php
+++ resources/views/components/container.blade.php
@@ -1,5 +1,5 @@
-<div class="md:p-5 p-0">
-    <div {{ $attributes->merge(['class' => 'shadow p-3 bg-gray-100 border md:border-teal-200 md:rounded'])}}>
+<div class="p-0 md:p-5">
+    <div {{ $attributes->merge(['class' => 'md:shadow md:p-3 md:bg-gray-100 md:border md:border-teal-200 md:rounded'])}}>
         {{ $slot }}
     </div>
 </div>
